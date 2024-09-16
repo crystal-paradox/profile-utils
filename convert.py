33fb27fbd962fb67f1c501f7a7bb557fb000f24a
Vladislav Belousov <vladbelousov@yahoo.com>
2024-09-16 11:06:17 +0100

    Generate the correct path do data.json

--- convert.py
+++ convert.py
@@ -187,7 +187,7 @@ class Converter:
 
     def save(self):
         self._update_dialogue_fragments()
-        file_path = os.path.join(os.getcwd(), 'profile', self.OUTPUT_FILE)
+        file_path = os.path.join(os.path.expanduser('~'), 'profile', 'assets', self.OUTPUT_FILE)
         with open(file_path, 'w', encoding='utf-8') as f:
             json.dump({
                 'project': self.project,
